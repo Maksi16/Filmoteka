72a9e4deee04dbfc5928beb484f27a7cae26631d
Maksi16 <maksim.moroz.kr@gmail.com>
2023-03-02 23:25:57 +0200

    copy project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/add-watch_queue';
+import './js/append-movie-cards';
+import './js/search-movies';
+import './js/button-up-down';
+import './js/fetch-movies';
+import './js/group-modal';
+import './js/open-film-modal';
---
+++ src/my-library.js
@@ -0,0 +1,9 @@
+import './js/active-button';
+import './js/add-watch_queue';
+import './js/append-my-library';
+import './js/button-up-down';
+import './js/fetch-movies';
+import './js/group-modal';
+import './js/open-film-modal';
+import './js/fetch-movies';
+import './js/renderMyLibraryWatched';
